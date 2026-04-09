bac4a4eb04ae63609080cb919cb707bfb5679398
chamod123756 <cmadusanka611@gmail.com>
2026-04-09 23:35:11 +0530

    feat: add EntityScan annotation to Application class for entity package scanning

--- backend/src/main/java/com/productivity/backend/Application.java
+++ backend/src/main/java/com/productivity/backend/Application.java
@@ -2,8 +2,10 @@ package com.productivity.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.productivity.backend.entity")
 public class Application {
 
 	public static void main(String[] args) {
